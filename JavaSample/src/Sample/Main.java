f1d5a449cebe769e798bf36cdbcf7472ddec634f
hiro1670 <hiroto0414@ezweb.ne.jp>
2023-12-14 17:41:10 +0900

    [1章-文字列の統合]

--- JavaSample/src/Sample/Main.java
+++ JavaSample/src/Sample/Main.java
@@ -4,7 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello Java");
+		String s1 = "Java and Javascript";
+		if (s1.contains("Java")) {
+			System.out.println("文字列s1はJavaを含んでいます");
+		}
+		if (s1.endsWith("Java")) {
+			System.out.println("文字列s1はJavaが末尾にあります");
+		}
+		System.out.println("文字列s1で最初にJavaが登場する位置は" + s1.indexOf("Java"));
 	}
-
 }
