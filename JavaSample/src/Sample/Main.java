9ec840165e7d53c3c348ba23409415fadf733878
hiro1670 <hiroto0414@ezweb.ne.jp>
2023-12-16 14:27:39 +0900

    [1章-正規表現の基本文法]

--- JavaSample/src/Sample/Main.java
+++ JavaSample/src/Sample/Main.java
@@ -3,14 +3,10 @@ package Sample;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		String s1 = "Java and Javascript";
-		if (s1.contains("Java")) {
-			System.out.println("文字列s1はJavaを含んでいます");
+		String s = "abc,def,ghi";
+		String[] words = s.split("[,:]");
+		for (String w : words) {
+			System.out.println(w + "->");
 		}
-		if (s1.endsWith("Java")) {
-			System.out.println("文字列s1はJavaが末尾にあります");
-		}
-		System.out.println("文字列s1で最初にJavaが登場する位置は" + s1.indexOf("Java"));
 	}
 }
